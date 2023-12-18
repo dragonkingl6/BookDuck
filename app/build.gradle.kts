5efffe40ad4db8c42e0dc637cc7fe8ad43857d8b
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-19 02:28:37 +0700

    Splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "duc.thanhhoa.bookduck"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "duc.thanhhoa.bookduck"
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +43,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/duc/thanhhoa/bookduck/MainActivity.java
+++ app/src/main/java/duc/thanhhoa/bookduck/MainActivity.java
@@ -3,12 +3,32 @@ package duc.thanhhoa.bookduck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import duc.thanhhoa.bookduck.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
