40929d353c4f5998fa503e9f484f92298e86e233
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-20 06:55:16 +0700

    delete show category

---
+++ app/src/main/java/duc/thanhhoa/bookduck/AdapterCategory.java
@@ -0,0 +1,126 @@
+package duc.thanhhoa.bookduck;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import duc.thanhhoa.bookduck.databinding.RowCategoryBinding;
+
+public class AdapterCategory extends RecyclerView.Adapter<AdapterCategory.HolderCategory> {
+
+    private Context context;
+    private ArrayList<ModelCategory> categoryList;
+
+    private RowCategoryBinding binding;
+
+    public AdapterCategory(Context context, ArrayList<ModelCategory> categoryList) {
+        this.context = context;
+        this.categoryList = categoryList;
+    }
+
+    @NonNull
+    @Override
+    public HolderCategory onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding= RowCategoryBinding.inflate(LayoutInflater.from(context), parent, false);
+
+        return new HolderCategory(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderCategory holder, int position) {
+
+        /* get data */
+
+        ModelCategory modelCategory= categoryList.get(position);
+        String id= modelCategory.getId();
+        String category= modelCategory.getCategory();
+        String uid= modelCategory.getUid();
+        String timestamp= modelCategory.getTimestamp();
+
+        //setdata
+        holder.categoryTv.setText(category);
+
+        holder.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(context);
+                builder.setTitle("Delete")
+                        .setMessage("Are you sure you want to delete category "+category+"?")
+                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //delete
+                                Toast.makeText(context, "Deleting...", Toast.LENGTH_SHORT).show();
+                                deleteCategory(modelCategory, holder);
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(context, "Cancelled", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+
+    private void deleteCategory(ModelCategory modelCategory, HolderCategory holder) {
+        //get id of category to delete
+        String id= modelCategory.getId();
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Categories");
+        ref.child(id)
+                .removeValue()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //deleted successfully
+                        Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context,""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    class HolderCategory extends RecyclerView.ViewHolder{
+
+        TextView categoryTv;
+        ImageButton deleteBtn;
+
+        public HolderCategory(@NonNull View itemView) {
+            super(itemView);
+
+            //init ui views
+
+            categoryTv= binding.categoryTv;
+            deleteBtn= binding.deleteBtn;
+        }
+    }
+}
--- app/src/main/java/duc/thanhhoa/bookduck/DashboardAdminActivity.java
+++ app/src/main/java/duc/thanhhoa/bookduck/DashboardAdminActivity.java
@@ -1,5 +1,6 @@
 package duc.thanhhoa.bookduck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,13 @@ import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import duc.thanhhoa.bookduck.databinding.ActivityDashboardAdminBinding;
 
@@ -17,6 +25,10 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
     private ActivityDashboardAdminBinding binding;
 
+    private ArrayList<ModelCategory> categoryList;
+
+    private AdapterCategory adapterCategory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,8 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
         firebaseAuth= FirebaseAuth.getInstance();
         checkUser();
+
+        loadCategory();
 
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +53,36 @@ public class DashboardAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(DashboardAdminActivity.this, CategoryActivity.class));
+            }
+        });
+    }
+
+    private void loadCategory() {
+        //init list
+        categoryList= new ArrayList<>();
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                categoryList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    ModelCategory modelCategory= ds.getValue(ModelCategory.class);
+                    //add to list
+                    categoryList.add(modelCategory);
+                }
+
+                //setup adapter
+
+                adapterCategory= new AdapterCategory(DashboardAdminActivity.this, categoryList);
+
+                //set adapter to recyclerview
+                binding.categoryRv.setAdapter(adapterCategory);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
