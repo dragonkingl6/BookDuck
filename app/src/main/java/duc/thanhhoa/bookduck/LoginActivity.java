aeae3350e9618372ab51e8bf691fdd86bbb0dda1
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-19 21:12:16 +0700

    create user

--- app/src/main/java/duc/thanhhoa/bookduck/LoginActivity.java
+++ app/src/main/java/duc/thanhhoa/bookduck/LoginActivity.java
@@ -2,13 +2,34 @@ package duc.thanhhoa.bookduck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import duc.thanhhoa.bookduck.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding= ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/duc/thanhhoa/bookduck/RegisterActivity.java
+++ app/src/main/java/duc/thanhhoa/bookduck/RegisterActivity.java
@@ -1,14 +1,163 @@
 package duc.thanhhoa.bookduck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import duc.thanhhoa.bookduck.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding= ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebaseAuth
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        //init progress dialog
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        binding.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+            }
+        });
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private String name= "", email= "", password= "";
+    private void validateData() {
+
+        //get data
+
+        name = binding.nameEt.getText().toString().trim();
+        email = binding.emailEt.getText().toString().trim();
+        password = binding.passwordEt.getText().toString().trim();
+        String confirmPassword = binding.confirmpasswordEt.getText().toString().trim();
+
+        //validate data
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this, "Enter your name...", Toast.LENGTH_SHORT).show();
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Invalid email pattern...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(confirmPassword)){
+            Toast.makeText(this, "Enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.length()<6){
+            Toast.makeText(this, "Password must be at least 6 characters long...", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals(confirmPassword)){
+            Toast.makeText(this, "Password doesn't match...", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            createUseraccount();
+        }
+    }
+
+    private void createUseraccount() {
+
+        progressDialog.setMessage("Creating account...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //account create
+                        updateUserInfo();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed creating account
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void updateUserInfo() {
+        progressDialog.setMessage("Saving user info...");
+
+        //timestamp
+        long timestamp= System.currentTimeMillis();
+
+        //get current user
+        String uid= firebaseAuth.getUid();
+
+        //setup data to add
+        HashMap<String, Object> hashMap= new HashMap<>();
+        hashMap.put("uid", ""+uid);
+        hashMap.put("email", ""+email);
+        hashMap.put("name", ""+name);
+        hashMap.put("profileImage", ""); //will add later
+        hashMap.put("userType", "user"); //will add later user, admin
+        hashMap.put("timestamp", ""+timestamp);
+
+        //save to db
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(uid)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //db updated
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Account created...", Toast.LENGTH_SHORT).show();
+                        //start profile activity
+                        startActivity(new Intent(RegisterActivity.this, DashboardUserActivity.class));
+                        finish();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 }
