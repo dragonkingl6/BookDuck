95d339106d913057169c938032ee4545c2833222
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-12-20 16:48:49 +0700

    uiux pdf list admin and model Pdf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //pdf
+    implementation ("com.github.barteksc:android-pdf-viewer:2.8.2")
+
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+
 }
---
+++ app/src/main/java/duc/thanhhoa/bookduck/activities/PdfListAdminActivity.java
@@ -0,0 +1,16 @@
+package duc.thanhhoa.bookduck.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import duc.thanhhoa.bookduck.R;
+
+public class PdfListAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_list_admin);
+    }
+}
